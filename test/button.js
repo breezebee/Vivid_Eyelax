2af3e650fc231a1130ce31d5cff904485f81ccb9
RaelJung <jungrael@RaelJungs-MacBook-Pro.local>
2021-02-03 19:45:32 +0900

    기본 구조

---
+++ test/button.js
@@ -0,0 +1,24 @@
+//돋보기 기능
+window.onload=function(){
+    var target = document.getElementById('magnify');
+    target.addEventListener('click', function(){
+        window.location.href='magnify.html';        //파일 이름 : magnify.html
+    })
+}
+
+//tts 기능
+window.onload=function(){
+    var target = document.getElementById('magnify');
+    target.addEventListener('click', function(){
+        window.location.href='tts.html';        //파일 이름 : tts.html
+    })
+}
+
+//글꼴 기능
+window.onload=function(){
+    var target = document.getElementById('magnify');
+    target.addEventListener('click', function(){
+        window.location.href='font.html';        //파일 이름 : font.html
+    })
+}
+
--- test/popup.js
+++
@@ -1,7 +0,0 @@
-window.onload=function(){
-    var target = document.getElementById('b1');
-    target.addEventListener('click', function(){
-        alert("Bbi Bbi");
-        window.location.href='font.html';
-    })
-}
