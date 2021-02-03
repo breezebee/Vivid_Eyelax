8c5c6ee53b83c496b1c3c20d3a07d0f2f1921412
RaelJung <jungrael@RaelJungs-MacBook-Pro.local>
2021-02-03 19:03:59 +0900

    html간 이동 성공

---
+++ test/popup.js
@@ -0,0 +1,7 @@
+window.onload=function(){
+    var target = document.getElementById('b1');
+    target.addEventListener('click', function(){
+        alert("Bbi Bbi");
+        window.location.href='font.html';
+    })
+}
